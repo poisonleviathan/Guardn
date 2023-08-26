c2fd40f088e6cbaed7d90fb1a5f2bd85c539a0ba
poisonleviathan <73103757+poisonleviathan@users.noreply.github.com>
2023-08-27 02:37:59 +0530

    Firebase Intergrated

--- app/src/main/java/com/example/guardn/Login.java
+++ app/src/main/java/com/example/guardn/Login.java
@@ -1,14 +1,120 @@
 package com.example.guardn;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class Login extends AppCompatActivity {
+    EditText SigninEmail, SigninPass;
+    Button SignUpRedirectBtn, SignInBtn;
 
+    FirebaseDatabase database;
+
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        SigninEmail = findViewById(R.id.Signin_Email);
+        SigninPass = findViewById(R.id.Signin_Pass);
+        SignInBtn = findViewById(R.id.SignIn_Btn);
+        SignUpRedirectBtn = findViewById(R.id.SignUpRedirect_btn);
+
+        SignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!validateEmail() | !validatePassword()){
+
+                }else{
+                    checkUser();
+                }
+            }
+        });
+        SignUpRedirectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(Login.this,Register.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public Boolean validateEmail(){
+        String val = SigninEmail.getText().toString();
+        if(val.isEmpty()){
+            SigninEmail.setError("Email Cannot be Empty");
+            return false;
+
+        }else {
+            SigninEmail.setError(null);
+            return true;
+        }
+    }
+
+    public Boolean validatePassword(){
+        String val = SigninPass.getText().toString();
+        if(val.isEmpty()){
+            SigninPass.setError("Password Cannot be Empty");
+            return false;
+
+        }else {
+            SigninPass.setError(null);
+            return true;
+        }
+    }
+
+    public void checkUser () {
+        String userEmail = SigninEmail.getText().toString().trim();
+        String userPassword =SigninPass.getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUserDatabase = reference.orderByChild("email").equalTo(userEmail);
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if (snapshot.exists()){
+                    SigninEmail.setError(null);
+                    String passwordFromDB = snapshot.child(userEmail).child("password").getValue(String.class);
+
+                    if (!Objects.equals(passwordFromDB,userPassword)){
+                        SigninEmail.setError(null);
+                        Intent intent =new Intent(Login.this, MainActivity.class);
+                        startActivity(intent);
+
+                    }else {
+                        SigninPass.setError("Invalid Credentials");
+                        SigninPass.requestFocus();
+
+                    }
+
+                } else {
+                    SigninEmail.setError("User Does Not Exist");
+                    SigninEmail.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/guardn/Register.java
+++ app/src/main/java/com/example/guardn/Register.java
@@ -2,13 +2,73 @@ package com.example.guardn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.ktx.Firebase;
 
 public class Register extends AppCompatActivity {
+
+    EditText SignUpUname, SignUpEmail, SignUpName, SignUpPass;
+    Button LoginRedirectbtn, SignUpbtn;
+
+    FirebaseDatabase database;
+
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
+
+
+        SignUpName = findViewById(R.id.SignUp_Name);
+        SignUpEmail = findViewById(R.id.SignUp_Email);
+        SignUpUname = findViewById(R.id.SignUp_Uname);
+        SignUpPass = findViewById(R.id.SignUp_Pass);
+        SignUpbtn = findViewById(R.id.SignUp_btn);
+        LoginRedirectbtn = findViewById(R.id.LoginRedirect_btn);
+
+        SignUpbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                database= FirebaseDatabase.getInstance();
+                reference= database.getReference("users");
+
+                String name = SignUpName.getText().toString();
+                String email = SignUpEmail.getText().toString();
+                String username = SignUpUname.getText().toString();
+                String password = SignUpPass.getText().toString();
+
+                HelperClass helperClass =new HelperClass (name,email,username,password);
+                reference.child(name).setValue(helperClass);
+
+               Toast.makeText(Register.this,"You Have SignUp SucccesFully",Toast.LENGTH_SHORT).show();
+                Intent intent =new Intent(Register.this, Login.class);
+                startActivity(intent);
+
+            }
+        });
+
+        LoginRedirectbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(Register.this, Login.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
